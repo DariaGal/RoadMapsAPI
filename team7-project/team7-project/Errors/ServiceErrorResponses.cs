274b77e538de5b56795bd1e34157435026475a5e
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-04-15 02:41:49 +0700

    added errors file.

---
+++ team7-project/team7-project/Errors/ServiceErrorResponses.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Models.Errors;
+
+namespace team7_project.Errors
+{
+    public static class ServiceErrorResponses
+    {
+        public static ServiceErrorResponse BodyIsMissing(string target)
+        {
+            var error = new ServiceErrorResponse
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Error = new ServiceError
+                {
+                    Code = ServiceErrorCodes.BadRequest,
+                    Message = "Request body is empty.",
+                    Target = target
+                }
+            };
+
+            return error;
+        }
+
+        public static ServiceErrorResponse UserNameAlreadyExists(string userName)
+        {
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+
+            var error = new ServiceErrorResponse
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Error = new ServiceError
+                {
+                    Code = ServiceErrorCodes.BadRequest,
+                    Message = $"User \"{userName}\" already exists.",
+                    Target = userName
+                }
+            };
+
+            return error;
+        }
+
+        public static ServiceErrorResponse NotEnoughUserData()
+        {
+            var error = new ServiceErrorResponse
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Error = new ServiceError
+                {
+                    Code = ServiceErrorCodes.ValidationError,
+                    Message = $"Username and / or password are not entered.",
+                    Target = "user"
+                }
+            };
+
+            return error;
+        }
+
+        public static ServiceErrorResponse UserNotFound()
+        {
+            var error = new ServiceErrorResponse
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Error = new ServiceError
+                {
+                    Code = ServiceErrorCodes.ValidationError,
+                    Message = $"There is no user with this login or password.",
+                    Target = "user"
+                }
+            };
+            return error;
+        }
+    }
+}
